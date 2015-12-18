98d6dd85acd43b92a9a0efcd17ccc40aee8fa9ff
Tim Claes <tim.claes@me.com>
2015-12-18 21:31:32 +0100

    Started creating voting models

--- ratings/models.py
+++ ratings/models.py
@@ -1,5 +1,30 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
 
 # Create your models here.
+
+class Score(models.Model):
+	'''
+	Een score voor een bijhorende content object
+	'''
+	content_type = models.ForeignKey(ContentType)
+
+
+
+class Vote(models.Model):
+	'''
+	Een enkele stem voor een bepaald content object, gelinkt aan een gebruiker
+	'''
+
+	content_type = models.ForeignKey(ContentType)
+	user = models.ForeignKey(User, related_name='votes')
+
+	class Meta:
+
+		# Een gebruiker kan maar 1 stem uitbrengen op een bepaald object
+		unique_together = (	
+			('content_type', 'object_id', 'user')
+		)
+
